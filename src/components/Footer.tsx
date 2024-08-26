8abc041da4eb2e8f350c112e853d38d14065aac0
unknown <danmaggot2@gmail.com>
2024-08-26 19:10:32 -0300

    :fix links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import {motion} from 'framer-motion'
+import { motion } from "framer-motion";
 import { DevIcon, GithubIcon, LinkedinIcon } from "@/icon/icon";
 import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="footer flex justify-between bg-neutral text-neutral-content items-center p-4">
       <aside className="flex items-center gap-2items-center text-sm">
-            <DevIcon/>
+        <DevIcon />
         <p className="text-xs">
           Daniel Rocha Copyright © {new Date().getFullYear()} - All right
           reserved
@@ -14,17 +14,18 @@ const Footer = () => {
       </aside>
       <nav className="grid-flow-col">
         <ul className="flex text-xl gap-2">
-          <li className="hover:brightness-125 cursor-pointer"
-      
-          >
-            <Link href={'https://github.com/Dannick10'} target="_blank">
-                <LinkedinIcon />
+          <li className="hover:brightness-125 cursor-pointer">
+            <Link href={"https://github.com/Dannick10"} target="_blank">
+              <GithubIcon />
             </Link>
           </li>
           <li className="hover:brightness-125 cursor-pointer">
-          <Link href={'https://www.linkedin.com/in/futurodevdaniel/'} target={'_blank'}>
-            <GithubIcon />
-          </Link>
+            <Link
+              href={"https://www.linkedin.com/in/futurodevdaniel/"}
+              target={"_blank"}
+            >
+              <LinkedinIcon />
+            </Link>
           </li>
         </ul>
       </nav>
